3ad6419d31f5a9d7e4716f6c78c6ab2bd529a752
shukawam <shukawam@gmail.com>
2023-04-17 15:49:47 +0900

    Update dto to record

--- services/product/src/main/java/me/shukawam/product/exception/ErrorResponse.java
+++ services/product/src/main/java/me/shukawam/product/exception/ErrorResponse.java
@@ -1,30 +1,7 @@
 package me.shukawam.product.exception;
 
-import jakarta.ws.rs.core.Response.StatusType;
+import jakarta.ws.rs.core.Response.Status;
 
-public class ErrorResponse {
-    private StatusType status;
-    private String message;
-
-    public ErrorResponse(StatusType status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public StatusType getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusType status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+public record ErrorResponse(Status status, String message) {
 
 }
